8132b2c998f6badcdadec6cecd85c07a4859199f
Luis0DEV <luisclaudionucci@gmail.com>
2024-12-27 16:47:22 -0300

    Update: Fix the project QUARKUS

--- src/main/java/com/project/controller/userController.java
+++ src/main/java/com/project/controller/userController.java
@@ -4,13 +4,12 @@ import com.project.entity.userEntity;
 
 import com.project.service.userService;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.UUID;
 
 @Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
@@ -22,8 +21,28 @@ public class userController {
         this.user_service = user_service;
     }
 
+    @GET
+    @Path("/{userId}")
+    public Response find_user(@PathParam("userId") UUID userId){
+        return Response.ok(user_service.filterUser(userId)).build();
+    }
+
     @POST
     public Response create_new_user(userEntity user) {
         return Response.status(Response.Status.CREATED).entity(user_service.createUser(user)).build();
     }
+
+    @PUT
+    @Path("/{userId}")
+    public Response update_user(@PathParam("userId") UUID userId, userEntity user) {
+        return Response.ok(user_service.updateUser(userId, user)).build();
+    }
+
+    @DELETE
+    @Path("/{userId}")
+    public Response delete_user(@PathParam("userId") UUID userId) {
+        user_service.delete_user(userId);
+        return Response.noContent().build();
+    }
 }
+
--- src/main/java/com/project/service/userService.java
+++ src/main/java/com/project/service/userService.java
@@ -2,11 +2,15 @@ package com.project.service;
 
 import com.project.entity.userEntity;
 
+import com.project.exception.customException;
+
 import com.project.repository.userRepository;
 
 import jakarta.enterprise.context.Dependent;
 
 import jakarta.transaction.Transactional;
+
+import java.util.UUID;
 
 @Dependent
 public class userService {
@@ -17,8 +21,33 @@ public class userService {
     }
 
     @Transactional
-    public userEntity createUser(userEntity user){
+    public userEntity filterUser(UUID userId) {
+        return user_repository.findByIdOptional(userId).orElseThrow(customException::new);
+    }
+
+    @Transactional
+    public userEntity createUser(userEntity user) {
+        if (user == null) {
+            throw new IllegalArgumentException("User entity cannot be null");
+        }
         user_repository.persist(user);
         return user;
+    }
+
+    @Transactional
+    public userEntity updateUser(UUID userId, userEntity user){
+        userEntity usuario = filterUser(userId);
+        usuario.setUserName(usuario.getUserName());
+        usuario.setUserUsername(usuario.getUserUsername());
+        usuario.setUserEmail(usuario.getUserEmail());
+        usuario.setUserPassword(usuario.getUserPassword());
+        usuario.setUserBirthday(usuario.getUserBirthday());
+        user_repository.persist(usuario);
+        return usuario;
+    }
+
+    @Transactional
+    public void delete_user(UUID userId) {
+        user_repository.delete(filterUser(userId));
     }
 }
